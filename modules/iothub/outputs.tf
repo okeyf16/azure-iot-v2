3b475d94c6a51870e7aaf5a09f14629c651346b7
okeyf16 <77342515+okeyf16@users.noreply.github.com>
2025-08-20 23:17:52 +0200

    Update outputs.tf

--- modules/iothub/outputs.tf
+++ modules/iothub/outputs.tf
@@ -3,3 +3,19 @@ output "listen_connection_string" { value = azurerm_eventhub_authorization_rule.
 output "eventhub_name"            { value = azurerm_eventhub.eh.name }
 output "namespace_name"           { value = azurerm_eventhub_namespace.ns.name }
 output "consumer_group"           { value = azurerm_eventhub_consumer_group.cg.name }
+
+output "name" {
+  value = azurerm_iothub.this.name
+}
+
+output "id" {
+  value = azurerm_iothub.this.id
+}
+
+output "route_name" {
+  value = azurerm_iothub_route.eh_route.name
+}
+
+output "endpoint_name" {
+  value = azurerm_iothub_endpoint_eventhub.eh_endpoint.name
+}
