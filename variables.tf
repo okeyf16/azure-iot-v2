a6459ad5d6b1481c76f0342ae1bfd3ce81868ead
okeyf16 <77342515+okeyf16@users.noreply.github.com>
2025-08-20 12:59:47 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,22 @@
+variable "ARM_CLIENT_ID" {
+  type        = string
+  description = "92243d42-ca4b-4baf-885e-1ed8a7542eb7"
+  sensitive   = true
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  type        = string
+  description = "9112a04a-6011-49f2-904c-f2b66b865b40"
+}
+
+variable "ARM_TENANT_ID" {
+  type        = string
+  description = "1bac489a-6766-481d-ace8-a72c1b526435"
+}
+
 variable "location" {
   description = "Azure region"
   type        = string
   default     = "North Europe"
 }
+
