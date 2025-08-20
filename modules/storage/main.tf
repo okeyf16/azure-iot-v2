9317cb1caeb32814d21a1846c3504ff5eb4719e6
okeyf16 <77342515+okeyf16@users.noreply.github.com>
2025-08-20 10:11:46 +0200

    Add files via upload

---
+++ modules/storage/main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_storage_account" "this" {
+  name                            = var.account_name
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  account_kind                    = "StorageV2"
+  min_tls_version                 = "TLS1_2"
+  allow_nested_items_to_be_public = false
+}
+
+resource "azurerm_storage_table" "tables" {
+  for_each             = toset(var.tables)
+  name                 = each.value
+  storage_account_name = azurerm_storage_account.this.name
+}
---
+++ modules/storage/variables.tf
@@ -0,0 +1,4 @@
+variable "account_name"        { type = string }
+variable "resource_group_name" { type = string }
+variable "location"            { type = string }
+variable "tables"              { type = list(string), default = [] }
