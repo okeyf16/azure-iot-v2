85147e23a0b51489a1525f135aac3f89fe97c6a3
okeyf16 <77342515+okeyf16@users.noreply.github.com>
2025-08-20 23:00:26 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,17 +1,8 @@
 terraform {
-  required_version = ">= 1.5.0"
-
-  cloud {
+  backend "remote" {
     organization = "Octurion"
     workspaces {
       name = "azure-iot-v2"
-    }
-  }
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.100"
     }
   }
 }
