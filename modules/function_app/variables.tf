9a7a2981bcbbf4fe56af5a86d27ff1ff317469c8
okeyf16 <77342515+okeyf16@users.noreply.github.com>
2025-08-20 22:42:56 +0200

    Update variables.tf

--- modules/function_app/variables.tf
+++ modules/function_app/variables.tf
@@ -1,16 +1,42 @@
-variable "name"                          { type = string }
-variable "resource_group_name"           { type = string }
-variable "location"                      { type = string }
+variable "name" {
+  type = string
+}
 
-variable "runtime_storage_account_name"  { type = string }
-variable "runtime_storage_account_key"   { type = string }
-variable "runtime_storage_connection_string" { type = string }
+variable "resource_group_name" {
+  type = string
+}
 
-variable "eh_listen_connection_string"   { type = string }
-variable "eventhub_name"                 { type = string }
-variable "eh_consumer_group"             { type = string }
+variable "location" {
+  type = string
+}
 
-variable "data_storage_account_id"       { type = string }
+variable "storage_account_name" {
+  type = string
+}
 
-variable "log_analytics_name"            { type = string }
-variable "app_insights_name"             { type = string }
+variable "storage_account_access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "eventhub_listen_conn_string" {
+  type      = string
+  sensitive = true
+}
+
+variable "eventhub_name" {
+  type = string
+}
+
+variable "table_name" {
+  type = string
+}
+
+variable "storage_account_id" {
+  type = string
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
