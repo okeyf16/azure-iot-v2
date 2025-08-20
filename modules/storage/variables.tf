dba105d519f464d71ef712bb71b4cb0964395992
okeyf16 <77342515+okeyf16@users.noreply.github.com>
2025-08-20 11:41:30 +0200

    Update variables.tf

--- modules/storage/variables.tf
+++ modules/storage/variables.tf
@@ -1,4 +1,8 @@
 variable "account_name"        { type = string }
 variable "resource_group_name" { type = string }
 variable "location"            { type = string }
-variable "tables"              { type = list(string), default = [] }
+variable "tables"              {
+  type = list(string)
+  default = [] 
+}
+
