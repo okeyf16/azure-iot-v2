6655bbf57025ec851b7643fa0000116b2d4cfb40
okeyf16 <77342515+okeyf16@users.noreply.github.com>
2025-08-20 14:36:55 +0200

    Update iothub.variables.tf

--- modules/iothub/variables.tf
+++ modules/iothub/variables.tf
@@ -1,21 +1,57 @@
-variable "resource_group_name" { type = string }
-variable "location"            { type = string }
-variable "namespace_name"      { type = string }
-variable "eventhub_name"       { type = string }
+variable "resource_group_name" {
+  description = local.rg_name
+  type = string
+}
+
+variable "location"            {
+  description = var.location
+  type = string
+}
+
+variable "namespace_name"      {
+  description = local.eh_namespace_name
+  type = string
+}
+
+variable "eventhub_name"       {
+  description = local.eh_name
+  type = string
+}
+
+variable "eh_endpoint_name" {
+  description = local.eh_endpoint_name
+  type        = string
+}
+
+variable "eh_route_name" {
+  description = local.eh_route_name 
+  type        = string
+}
+
+variable "eh_send_connection_string" {
+  description = module.eventhub.send_connection_string
+  type        = string
+  sensitive   = true
+}
+
 variable "throughput_units"    {
   type = number
   default = 1 
 }
+
 variable "partitions"          {
   type = number
   default = 2
 }
+
 variable "retention_days"      {
   type = number
   default = 1 
 }
+
 variable "consumer_group"      {
   type = string
   default = "func" 
 }
+
 
