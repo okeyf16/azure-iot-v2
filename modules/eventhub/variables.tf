a7985e6c3f1950d9d298e2dda75ad60a80df73ab
okeyf16 <77342515+okeyf16@users.noreply.github.com>
2025-08-20 11:38:27 +0200

    Update variables.tf

--- modules/eventhub/variables.tf
+++ modules/eventhub/variables.tf
@@ -2,7 +2,20 @@ variable "resource_group_name" { type = string }
 variable "location"            { type = string }
 variable "namespace_name"      { type = string }
 variable "eventhub_name"       { type = string }
-variable "throughput_units"    { type = number, default = 1 }
-variable "partitions"          { type = number, default = 2 }
-variable "retention_days"      { type = number, default = 1 }
-variable "consumer_group"      { type = string, default = "func" }
+variable "throughput_units"    { 
+  type = number 
+  default = 1 
+}
+variable "partitions"          {
+  type = number
+  default = 2 
+}
+variable "retention_days"      {
+  type = number
+  default = 1 
+}
+variable "consumer_group"      {
+  type = string
+  default = "func" 
+}
+
