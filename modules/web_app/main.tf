c78536f6e61e546c3c0eb5f09a76358de598296a
okeyf16 <77342515+okeyf16@users.noreply.github.com>
2025-09-21 16:42:13 +0200

    Update main.tf

--- modules/web_app/main.tf
+++ modules/web_app/main.tf
@@ -1,7 +1,7 @@
 #######################
 # App Service Plan    #
 #######################
-resource "azurerm_app_service_plan" "this" {
+resource "azurerm_service_plan" "this" {
   name                = var.plan_name
   location            = var.location
   resource_group_name = var.resource_group_name
