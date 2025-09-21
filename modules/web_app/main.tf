23cdae5904799b7945f72a1bc21cb4e0123dee91
okeyf16 <77342515+okeyf16@users.noreply.github.com>
2025-09-21 18:03:17 +0200

    Update main.tf

--- modules/web_app/main.tf
+++ modules/web_app/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_service_plan" "this" {
   location            = var.location
   resource_group_name = var.resource_group_name
   os_type = "Linux" 
-  sku_name = "F1"
+  sku_name = "B1"
   }
 
 #######################
