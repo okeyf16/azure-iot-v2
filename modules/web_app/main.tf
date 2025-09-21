2190dbf0c5c6589786dc1fbd19b40b29911d313c
okeyf16 <77342515+okeyf16@users.noreply.github.com>
2025-09-21 16:22:20 +0200

    Create main.tf

---
+++ modules/web_app/main.tf
@@ -0,0 +1,36 @@
+#######################
+# App Service Plan    #
+#######################
+resource "azurerm_app_service_plan" "this" {
+  name                = var.plan_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+#######################
+# Web App             #
+#######################
+resource "azurerm_app_service" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  app_service_plan_id = azurerm_app_service_plan.this.id
+
+  site_config {
+    always_on = false # Free tier does not support Always On
+  }
+
+  app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY"        = var.app_insights_key
+    "APPLICATIONINSIGHTS_CONNECTION_STRING" = var.app_insights_conn_string
+    "STORAGE_CONN_STR"                      = var.storage_conn_str
+    "API_KEY"                               = var.api_key
+  }
+
+  tags = var.tags
+}
