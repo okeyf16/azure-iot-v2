db4a1f8f34ac68c3394e8a3a7006d00e61d3b406
okeyf16 <77342515+okeyf16@users.noreply.github.com>
2025-09-21 14:10:33 +0200

    Update outputs.tf

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -2,3 +2,5 @@ output "id"                        { value = azurerm_storage_account.this.id }
 output "name"                      { value = azurerm_storage_account.this.name }
 output "primary_connection_string" { value = azurerm_storage_account.this.primary_connection_string }
 output "primary_access_key"        { value = azurerm_storage_account.this.primary_access_key }
+output "telemetry_table_name"      { value = azurerm_storage_table.telemetry.name }
+
