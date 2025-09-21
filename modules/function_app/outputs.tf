78587e6a6515277e9a67b6655aab29044ccb6ec1
okeyf16 <77342515+okeyf16@users.noreply.github.com>
2025-09-21 17:43:50 +0200

    Update outputs.tf

--- modules/function_app/outputs.tf
+++ modules/function_app/outputs.tf
@@ -1,5 +1,6 @@
 output "name"                             { value = azurerm_linux_function_app.func.name }
 output "principal_id"                     { value = azurerm_linux_function_app.func.identity[0].principal_id }
-output "app_insights_instrumentation_key" { value = azurerm_application_insights.appinsights.instrumentation_key }
-output "app_insights_connection_string"   { value = azurerm_application_insights.appinsights.connection_string }
+output "app_insights_instrumentation_key" { value = azurerm_application_insights.ai.instrumentation_key }
+output "app_insights_connection_string"   { value = azurerm_application_insights.ai.connection_string }
+
 
